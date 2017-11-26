19ff48f6b7ca67d0b6bee02633376c0fdf1b8b9c
Paul McCumber <pmccumber@gmail.com>
2017-11-26 14:45:45 -0700

    Initial working Naive Bayesian Classifier with 10,000 reviews pulled for Phoenix, Scottsdale, Mesa, and Las Vegas.  Also included results.

---
+++ scripts/paulsWork/getReviewsCity.py
@@ -0,0 +1,43 @@
+#!/opt/local/bin/python2.7
+
+import sys
+import random
+import YelpDB
+
+### Ensure an argument was given 
+if len(sys.argv) != 2:
+   print "\n *** Please provide the city to select reviews from *** \n"
+   sys.exit (0)
+
+### Connect to the database and get all the IDs
+ydb       = YelpDB.YelpDB()
+reviewIDs = ydb.getReviewIDs()
+
+### This will contain a hash of the review ids selected
+selected = {}
+
+### This is a list of the business categories we accept
+restaurants = []
+restaurants.append('Restaurants')
+
+### Loop until we've pulled the limit number of review IDs
+for id in reviewIDs:
+
+    ### Pull the review, then the business, and the business category
+    (stars, date, text, business_id, user_id) = ydb.getReview(id)
+    (name, city, state) = ydb.getBusiness (business_id)
+    (category)          = ydb.getCategory (business_id)
+
+    ### If the review has not been selected and it is in the lis
+    ### of acceptable categories, keep it.
+    try:
+        if id not in selected and category[0] == 'Restaurants' and city == sys.argv[1]:
+            selected[id] = 1 
+            if len(selected)>=10000:
+               break
+    except:
+        continue
+
+### When done, dump all the IDs
+for id in selected:
+    print id[0]
---
+++ scripts/paulsWork/naiveBayesClassifier.py
@@ -0,0 +1,117 @@
+#!/opt/local/bin/python2.7 
+import sys
+import pdb
+import nltk
+import re
+import random
+
+import YelpDB
+
+class NaiveBayes ():
+
+    ### This class exists purely to override Process from the base
+    def __init__ (self, fileName):
+        self.ydb       = YelpDB.YelpDB()
+        self.file      = open (fileName)
+        self.reviewIDs     = []
+        self.processedText = []
+
+    ### Override the process() to accumulate a training set of the unigrams
+    ### bigrams
+    #
+    def process (self, stars, text, business_id, user_id):
+       
+        stopwords = nltk.corpus.stopwords.words("english")
+        np = r"(\w)"
+        
+        processedText = []
+        
+        if stars > 3:
+           score = "positive"
+        else:
+           score = "negative"
+
+        filteredStopWords = []
+        try:
+            words = nltk.word_tokenize(text)
+            words = [word.lower() for word in words]
+            for t in  words:
+                if t not in stopwords:
+                    filteredStopWords.append(t)
+
+            filteredComplete = []
+            for t in filteredStopWords:
+                result = re.search(np, t)
+                if result != None:
+                    filteredComplete.append(t)
+ 
+            thisVector = (text, filteredComplete, score)
+            self.processedText.append(thisVector)
+
+        except:
+            pass
+            #print "Review failed to tokenize"
+
+    ### Override loop() too (oh well, it got me a long way down the road :) )
+    #
+    def loop (self, reviewList ):
+        for nextID in reviewList:
+            (stars, date, text, business_id, user_id) = self.ydb.getReview (nextID)
+            self.process (stars, text, business_id, user_id)
+
+    def readReviewIDs (self):
+        i = 0
+        for nextID in self.file:
+            nextID = nextID.rstrip()
+            self.reviewIDs.append(nextID)
+            i = i + 1
+
+    def buildFeatures (self, reviewText, reviewWords):
+
+        featureVector = {}
+
+        unigramDistribution   = nltk.FreqDist(reviewWords)
+        for word, frequency in unigramDistribution.items():
+            featureVector[word] = frequency
+
+        bigrams               = list(nltk.bigrams(reviewWords))
+        bigramDistribution    = nltk.FreqDist(bigrams)
+        for word, frequency in bigramDistribution.items():
+            featureVector[word] = frequency
+
+        return featureVector
+ 
+### Get the filename with the review IDs
+if len(sys.argv) < 2:
+    print "\nProvide training and test files as arguments\n"
+    sys.exit (-1)
+
+### Get and instance and process the file
+processor = NaiveBayes (sys.argv[1])
+processor.readReviewIDs()
+
+### Randomize the reviews
+random.shuffle(processor.reviewIDs)
+
+trainingListEnd = int(len(processor.reviewIDs) * 0.67)
+processor.loop(processor.reviewIDs[:trainingListEnd])
+
+trainingFeatureSet = []
+for (text, filteredWords, score) in processor.processedText:
+    featureVector = processor.buildFeatures(text, filteredWords)
+    trainingFeatureSet.append ((featureVector, score))
+
+### Better clear the list of processed reviews from training
+processor.processedText = []
+processor.loop(processor.reviewIDs[trainingListEnd:])
+testFeatureSet = []
+for (text, filteredWords, score) in processor.processedText:
+    featureVector = processor.buildFeatures(text, filteredWords)
+    testFeatureSet.append ((featureVector, score))
+
+classifier = nltk.NaiveBayesClassifier.train (trainingFeatureSet)
+accuracy = nltk.classify.accuracy(classifier, testFeatureSet)
+bestFeatures = classifier.most_informative_features(500)
+
+print "\nAccuracy of %5.3f\n" % accuracy
+classifier.show_most_informative_features(100)
